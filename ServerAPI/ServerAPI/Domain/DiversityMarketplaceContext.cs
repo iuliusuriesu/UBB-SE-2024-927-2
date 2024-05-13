14c793d2c8023c3223af05c181c755f215460c73
Iulius Uriesu <115088577+iulius999@users.noreply.github.com>
2024-05-13 21:15:02 +0300

    Created Context

---
+++ ServerAPI/ServerAPI/Domain/DiversityMarketplaceContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ServerAPI.Domain
+{
+    public class DiversityMarketplaceContext : DbContext
+    {
+        public DiversityMarketplaceContext(DbContextOptions<DiversityMarketplaceContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ServerAPI/ServerAPI/Domain/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ServerAPI.Domain
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+    }
+}
